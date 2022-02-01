d5e20c9e16689810f84bd41b0d5b734aeda96995
Prithiviraj-R <95839811+Prithiviraj-R@users.noreply.github.com>
2022-02-01 08:09:59 +0530

    Add files via upload

--- src/mainlayertask2/OutputLayerTask2.java
+++ src/mainlayertask2/OutputLayerTask2.java
@@ -1,9 +1,35 @@
 package mainlayertask2;
+import newexception.MistakeOccuredException;
 import task2.StringBuilderTask;
 import java.util.Scanner;
 public class OutputLayerTask2
 {
-public static void main(String[] args)
+private static StringBuilder commonCase(StringBuilder builder) throws Exception
+{
+    StringBuilder str=null;
+    StringBuilderTask task2=new StringBuilderTask();
+    Scanner input=new Scanner(System.in);
+    System.out.println("How many Strings you want after firstOne:");
+    int inputNumber=Integer.parseInt(input.nextLine());
+    if(inputNumber<=0)
+    {
+       System.out.println("number should not be zero or negative");
+    }
+    else
+    {
+    String[] inputArray=new String[inputNumber];
+    for(int i=0;i<inputNumber;i++)
+    {
+       System.out.println("Enter the Strings");
+       inputArray[i]=input.nextLine();
+    } 
+      System.out.println("Enter the symbol");
+      String symbol1=input.nextLine();
+      str=task2.insertSymbol(builder,inputArray,symbol1);
+    }  
+    return str;
+}
+public static void main(String[] args) throws Exception
 {
 StringBuilderTask task2=new StringBuilderTask();
 
@@ -27,6 +53,10 @@ case 1:
         int lengthOfLoadedOne=task2.lengthOfStringBuilder(loadedone);
         System.out.println("length of New builder:"+lengthOfLoadedOne);
     }
+    catch(MistakeOccuredException error)
+    {
+        System.out.println(error.getMessage());
+    }
     catch(Exception error_1)
     {
         System.out.println("Enter the valid input"+error_1.getMessage());
@@ -36,37 +66,18 @@ case 1:
 case 2:
  try
     {
-    System.out.println("Enter the String:");
-    String inputString2=input.nextLine();
-    StringBuilder withLoadedString=task2.createStringBuilderWithInput(inputString2);
-    System.out.println("StringBuilder with string:"+withLoadedString);
-    System.out.println("Length of the Stringbuilder:"+task2.lengthOfStringBuilder(withLoadedString));
-    System.out.println("Enter the Number");
-    int inputNumber=Integer.parseInt(input.nextLine());
-    if(inputNumber<=0)
+      System.out.println("Enter the string:");
+      String inputString2=input.nextLine();
+      StringBuilder withLoadedString=task2.createStringBuilderWithInput(inputString2);
+      System.out.println("StringBuilder with string:"+withLoadedString);
+      System.out.println("Length of the Stringbuilder:"+task2.lengthOfStringBuilder(withLoadedString));
+      StringBuilder stringBuilderOutput=commonCase(withLoadedString);
+      System.out.println(stringBuilderOutput);
+      System.out.println("Length:"+task2.lengthOfStringBuilder(stringBuilderOutput));
+    }
+    catch(MistakeOccuredException error)
     {
-       System.out.println("number should not be zero or negative");
-    }
-    else
-    {
-    String[] inputArray=new String[inputNumber];
-    for(int i=0;i<inputNumber;i++)
-    {
-       System.out.println("Enter the Strings");
-       inputArray[i]=input.nextLine();
-    }
-    try
-    { 
-      System.out.println("Enter the symbol");
-      String symbol1=input.nextLine();
-      System.out.println(task2.insertSymbol(withLoadedString,inputArray,symbol1));
-      System.out.println("Length:"+task2.lengthOfStringBuilder(withLoadedString));
-    }
-    catch(Exception error_2)
-    {
-         System.out.println("Enter The Valid input."+error_2.getMessage());
-    }
-    }
+        System.out.println(error.getMessage());
     }
     catch(Exception error2)
     {
@@ -106,6 +117,10 @@ case 3:
     System.out.println("length after:"+task2.lengthOfStringBuilder(str1));
     }
     }
+    catch(MistakeOccuredException error)
+    {
+        System.out.println(error.getMessage());
+    }
     catch(Exception error_3)
     {
        System.out.println("Enter the valid Input."+error_3.getMessage());
@@ -117,25 +132,14 @@ try
     System.out.println("Enter the string:");
     String case4inputString1=input.nextLine();
     StringBuilder str2=task2.createStringBuilderWithInput(case4inputString1);
-    System.out.println("How many Strings you want?");
-    int inputNumber4=Integer.parseInt(input.nextLine());
-    if(inputNumber4<=0)
-    {
-       System.out.println("number should not be zero or negative");
+    StringBuilder strBuilder1=commonCase(str2);
+    System.out.println("Length before:"+task2.lengthOfStringBuilder(strBuilder1));
+    System.out.println(task2.deleteString(case4inputString1,strBuilder1));
+    System.out.println("Length after:"+task2.lengthOfStringBuilder(strBuilder1));
     }
-    else
+    catch(MistakeOccuredException error)
     {
-    String[] inputArrayForCase4=new String[inputNumber4];
-    for(int i=0;i<inputNumber4;i++)
-    {
-       System.out.println("Enter the Strings");
-       inputArrayForCase4[i]=input.nextLine();
-    }
-    System.out.println("StringBuilder with three inputs:"+task2.insertSymbol(str2,inputArrayForCase4," "));
-    System.out.println("Length before:"+task2.lengthOfStringBuilder(str2));
-    System.out.println(task2.deleteString(case4inputString1,str2));
-    System.out.println("Length after:"+task2.lengthOfStringBuilder(str2));
-    }
+        System.out.println(error.getMessage());
     }
     catch(Exception error_3)
     {
@@ -172,6 +176,10 @@ case 5:
     System.out.println("Length:"+task2.lengthOfStringBuilder(str4));
     }
     }
+    catch(MistakeOccuredException error)
+    {
+        System.out.println(error.getMessage());
+    }
     catch(Exception error_4)
     {
        System.out.println("Enter the valid input."+error_4.getMessage());
@@ -183,28 +191,15 @@ case 6:
     {
     System.out.println("Enter the string:");
     String case6string1=input.nextLine();
-    StringBuilder str5=task2.createStringBuilderWithInput(case6string1);
-    System.out.println("how many Strings you want:");
-    int inputNumber6=Integer.parseInt(input.nextLine());
-    System.out.println("which symbol is in between the Strings:");
-    String case6Symbol=input.nextLine();
-    if(inputNumber6<=0)
-    {
-       System.out.println("number should not be zero or negative");
-    }
-    else
-    {
-    String[] inputArrayCase6=new String[inputNumber6];
-    for(int i=0;i<inputNumber6;i++)
-    {
-       System.out.println("Enter the Strings");
-       inputArrayCase6[i]=input.nextLine();
-    }
-    System.out.println("StringBuilder with three inputs:"+task2.insertSymbol(str5,inputArrayCase6,case6Symbol));
+    StringBuilder strBuilder6=task2.createStringBuilderWithInput(case6string1);
+    StringBuilder str5=commonCase(strBuilder6);
     System.out.println("Length of the Stringbuilder:"+task2.lengthOfStringBuilder(str5));
     System.out.println(task2.reverseString(str5));
     System.out.println("Length after:"+task2.lengthOfStringBuilder(str5));
     }
+    catch(MistakeOccuredException error)
+    {
+        System.out.println(error.getMessage());
     }
     catch(Exception error_6)
     {
@@ -221,18 +216,15 @@ case 7:
     int case7Number1=Integer.parseInt(input.nextLine());
     System.out.println("Enter the EndingPosition:");
     int case7Number2=Integer.parseInt(input.nextLine());
-    try
-    {
     StringBuilder str7=task2.createStringBuilderWithInput(case7String1);
     System.out.println("String builder with single input:"+str7);
     System.out.println("Length of the stringbuilder:"+task2.lengthOfStringBuilder(str7));
     System.out.println(task2.deleteCharacters(str7,case7Number1-1,case7Number2));
     System.out.println("length after:"+task2.lengthOfStringBuilder(str7));
     }
-    catch(Exception error_7)
+    catch(MistakeOccuredException error)
     {
-      System.out.println("Enter valid input."+error_7.getMessage());
-    }
+        System.out.println(error.getMessage());
     }
     catch(Exception error_7)
     {
@@ -251,18 +243,15 @@ case 8:
     int case8Number1=Integer.parseInt(input.nextLine());
     System.out.println("Enter the EndingPosition:");
     int case8Number2=Integer.parseInt(input.nextLine());
-    try
-    {
     StringBuilder str7=task2.createStringBuilderWithInput(case8String1);
     System.out.println("stringbuilder with minimum 10 characters:"+str7);
     System.out.println("Length"+task2.lengthOfStringBuilder(str7));
     System.out.println(task2.replaceCharacters(str7,case8String2,case8Number1-1,case8Number2));
     System.out.println("length after:"+task2.lengthOfStringBuilder(str7));
     }
-    catch(Exception error_8)
+   catch(MistakeOccuredException error)
     {
-       System.out.println("Enter the valid Input."+error_8.getMessage());
-    }
+        System.out.println(error.getMessage());
     }
     catch(Exception error_8)
     {
@@ -275,22 +264,16 @@ case 9:
     {
     System.out.println("Enter the string:");
     String case9String1=input.nextLine();
-    StringBuilder str8=task2.createStringBuilderWithInput(case9String1);
-    System.out.println("Enter the number of strings after the first String:");
-    int inputNumber9=Integer.parseInt(input.nextLine());
-    String[] inputArrayForCase9=new String[inputNumber9];
-    for(int i=0;i<inputNumber9;i++)
-    {
-       System.out.println("Enter the Strings");
-       inputArrayForCase9[i]=input.nextLine();
-    }
-    System.out.println("Symbol or letter Which has to be in between the Strings:");
-    String case9Symbol=input.nextLine();
-    System.out.println("StringBuilder with Desired input:"+task2.insertSymbol(str8,inputArrayForCase9,case9Symbol));
+    StringBuilder strBuilder9=task2.createStringBuilderWithInput(case9String1);
+    StringBuilder str8=commonCase(strBuilder9);
     System.out.println("Length of the Stringbuilder:"+task2.lengthOfStringBuilder(str8));
     System.out.println("Which one First occurance you want?");
     String case9Symbol2=input.nextLine();
     System.out.println("Index number of the asked one:"+task2.indexOfString(str8,case9Symbol2));
+    }
+    catch(MistakeOccuredException error)
+    {
+        System.out.println(error.getMessage());
     }
     catch(Exception error_9)
     {
@@ -302,22 +285,17 @@ try
     {
     System.out.println("Enter the string:");
     String case10String1=input.nextLine();
-    StringBuilder str9=task2.createStringBuilderWithInput(case10String1);
-    System.out.println("Enter the number of strings after the first String:");
-    int inputNumber10=Integer.parseInt(input.nextLine());
-    String[] inputArrayForCase10=new String[inputNumber10];
-    for(int i=0;i<inputNumber10;i++)
-    {
-       System.out.println("Enter the Strings");
-       inputArrayForCase10[i]=input.nextLine();
-    }
-    System.out.println("Symbol or letter Which has to be in between the Strings:");
-    String case10Symbol=input.nextLine();
-    System.out.println("Stringbuilder with symbol:"+task2.insertSymbol(str9,inputArrayForCase10,case10Symbol));
+    StringBuilder strBuilder10=task2.createStringBuilderWithInput(case10String1);
+    StringBuilder str9=commonCase(strBuilder10);
+    System.out.println(str9);
     System.out.println("Length of the Stringbuilder:"+task2.lengthOfStringBuilder(str9));
     System.out.println("Which one's lastOccurance you want");
     String case10Symbol2=input.nextLine();
     System.out.println(task2.lastIndexOfString(str9,case10Symbol2));
+    }
+    catch(MistakeOccuredException error)
+    {
+        System.out.println(error.getMessage());
     }
     catch(Exception error_10)
     {
